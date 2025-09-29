bea39fefdafd78b0c2e7253af338dec424786337
Allan <allanjaybusante3@gmail.com>
2025-09-29 12:00:56 +0800

    feat(checkout): add confirmation modal before payment

--- store/src/pages/Checkout.jsx
+++ store/src/pages/Checkout.jsx
@@ -1,10 +1,13 @@
 import "../css/Checkout.css"
+import { useState } from "react";
 import CartProductCard from "../components/CartProductCard";
 import { useCart } from "../contexts/CartContext";
 import { useNavigate } from "react-router-dom";
+import ConfirmModal from "../components/ConfirmModal";
 
 function Checkout() {
     const {cart, setCart, checkoutItems, setCheckoutItems} = useCart();
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
     const navigate = useNavigate();
 
     const totalPrice = checkoutItems.reduce((total, items) => total + items.price, 0);
@@ -19,15 +22,27 @@ function Checkout() {
 
     return <div className="checkout">
         {checkoutItems.map(item => (
-            <CartProductCard product={item} key={item.id}/>
+            <div className="checkout-card" key={item.id}>
+                <CartProductCard product={item}/>
+            </div>
         ))}
 
         <div className="payment-details">
             <h3>Total Price: ${totalPrice.toFixed(2)}</h3>
             <h3>Tax(10%) : ${tax.toFixed(2)}</h3>
             <h2>Total Price after tax: ${totalPriceWithTax.toFixed(2)}</h2>
-            <button onClick={payItems}>Pay</button>
+            
+            <button onClick={() => navigate("/cart")}>Cancel</button>
+            <button onClick={() => setShowConfirmModal(true)}>Pay</button>
         </div>
+
+        {showConfirmModal && 
+            <ConfirmModal 
+                onCancel={() => setShowConfirmModal(false)} 
+                onConfirm={() => {payItems(), setShowConfirmModal(false)}}
+            />
+        }
+        
     </div>
 }
 
